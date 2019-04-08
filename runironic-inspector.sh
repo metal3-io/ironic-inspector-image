cac2da528c3216fbffef61d54a5eecd25ee72d5f
Bob Fournier <bfournie@redhat.com>
2019-04-07 20:22:17 -0400

    Add iptables rule for ironic inspector

--- runironic-inspector.sh
+++ runironic-inspector.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/bash
+
+INTERFACE=${INTERFACE:-"provisioning"}
+
+# Allow access to Ironic inspector API
+if ! iptables -C INPUT -i $INTERFACE -p tcp -m tcp --dport 5050 -j ACCEPT > /dev/null 2>&1; then
+    iptables -I INPUT -i $INTERFACE -p tcp -m tcp --dport 5050 -j ACCEPT
+fi
+
 /usr/bin/python2 /usr/bin/ironic-inspector --config-file /etc/ironic-inspector/inspector-dist.conf --config-file /etc/ironic-inspector/inspector.conf
