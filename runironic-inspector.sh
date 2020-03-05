2107ad50b866fc7e383c7cf0f9ae9933e5e011d0
Riccardo Pittau <elfosardo@gmail.com>
2020-03-05 15:34:46 +0100

    Stop logging to file

--- runironic-inspector.sh
+++ runironic-inspector.sh
@@ -6,16 +6,10 @@ CONFIG=/etc/ironic-inspector/inspector.conf
 
 wait_for_interface_or_ip
 
-# Remove log files from last deployment
-rm -rf /shared/log/ironic-inspector
-
-mkdir -p /shared/log/ironic-inspector
-
 cp $CONFIG $CONFIG.orig
 
 crudini --set $CONFIG ironic endpoint_override http://$IRONIC_URL_HOST:6385
 crudini --set $CONFIG service_catalog endpoint_override http://$IRONIC_URL_HOST:5050
 
 exec /usr/bin/ironic-inspector --config-file /etc/ironic-inspector/inspector-dist.conf \
-	--config-file /etc/ironic-inspector/inspector.conf \
-	--log-file /shared/log/ironic-inspector/ironic-inspector.log
+	--config-file /etc/ironic-inspector/inspector.conf
