8180263fcd338d79812c7e5604f4d3566588eae4
Riccardo Pittau <elfosardo@gmail.com>
2021-01-04 11:40:30 +0100

    Allow specify package versions
    With this change we can specify package versions to be installed
    directly in the packages list (main or extra).

--- prepare-image.sh
+++ prepare-image.sh
@@ -5,7 +5,7 @@ set -ex
 dnf install -y python3 python3-requests
 curl https://raw.githubusercontent.com/openstack/tripleo-repos/master/tripleo_repos/main.py | python3 - -b master current-tripleo
 dnf upgrade -y
-dnf install -y $(cat /tmp/main-packages-list.txt)
+xargs -rtd'\n' dnf --setopt=install_weak_deps=False install -y < /tmp/main-packages-list.txt
 mkdir -p /var/lib/ironic-inspector
 sqlite3 /var/lib/ironic-inspector/ironic-inspector.db "pragma journal_mode=wal"
 dnf remove -y sqlite
