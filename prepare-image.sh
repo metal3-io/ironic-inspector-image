81d1f1ce57fc49583302c984512f9fd0fb57f42c
Nam Xuan Nguyen <nam.xuan.nguyen@est.tech>
2021-03-25 19:49:02 +0200

    Reload httpd reverse proxy when the certificate is updated

--- prepare-image.sh
+++ prepare-image.sh
@@ -2,9 +2,11 @@
 
 set -euxo pipefail
  
-dnf install -y python3 python3-requests
-curl https://raw.githubusercontent.com/openstack/tripleo-repos/master/tripleo_repos/main.py | python3 - -b master current-tripleo
+dnf install -y python3 python3-requests epel-release
+
+curl https://raw.githubusercontent.com/openstack/tripleo-repos/master/tripleo_repos/main.py | python3 - -b master current-tripleo --no-stream
 dnf upgrade -y
+
 xargs -rtd'\n' dnf --setopt=install_weak_deps=False install -y < /tmp/${PKGS_LIST}
 if [[ ! -z ${EXTRA_PKGS_LIST:-} ]]; then
     if [[ -s /tmp/${EXTRA_PKGS_LIST} ]]; then
